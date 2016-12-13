e25d4f458f7628259de7cdaf503b34ec484ac9c7
vagrant <vagrant@vagrant.vm>
2016-12-13 05:55:29 +0000

    Hackerrank days 0 and 1

---
+++ dec_12.py
@@ -0,0 +1,37 @@
+# Hackerrank Day 0 - Hello World
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = raw_input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print 'Hello, World.'
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print input_string
+
+########################################################################
+
+# Hackerrank Day 1 - Data Types
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+i2 = raw_input()
+d2 = raw_input()
+s2 = raw_input()
+
+# Print the sum of both integer variables on a new line.
+print i + int(i2)
+
+# Print the sum of the double variables on a new line.
+print d + float(d2)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + str(s2)
+
+########################################################################
